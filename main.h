8b16f52bcb19f18dc957a6740d505b24199b49e2
Nathan <nathan.benavides@epfl.ch>
2022-04-28 11:10:28 +0200

    PI pos et dist

--- main.h
+++ main.h
@@ -22,9 +22,7 @@ extern "C" {
 #define GOAL_DISTANCE 			10.0f
 #define MAX_DISTANCE 			25.0f
 #define ERROR_THRESHOLD			0.1f	//[cm] because of the noise of the camera
-#define KP						800.0f
-#define KI 						3.5f	//must not be zero
-#define MAX_SUM_ERROR 			(MOTOR_SPEED_LIMIT/KI)
+
 
 /** Robot wide IPC bus. */
 extern messagebus_t bus;
