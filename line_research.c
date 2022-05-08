9a9903c9ff8d0d33356109da760acad96e7990d8
Nathan <nathan.benavides@epfl.ch>
2022-05-08 17:21:12 +0200

    implémentation fonction dans FSM

--- line_research.c
+++ line_research.c
@@ -14,50 +14,33 @@
 static thread_t *findLineThd;
 static uint8_t findLine_configured = 0;
 
-static bool search_line = 0;
-static bool line_found = 0;
-
-
 static THD_WORKING_AREA(waFindLine, 256);
 static THD_FUNCTION(FindLine, arg) {
 
     chRegSetThreadName(__FUNCTION__);
     (void)arg;
-    systime_t time = chVTGetSystemTime();
 
-    while(1){ //faire un bool modifié par motor_control pour dire quand il cherche la ligne
-    	if (search_line){
-    		set_motor_control_active(0);	//stops random movements
+    systime_t time_search = chVTGetSystemTime();
+    static bool line_found = 0;
 
-			systime_t time_search = chVTGetSystemTime();
+    while(1){
+    	right_motor_set_speed(LOW_SPEED);
+		left_motor_set_speed(-LOW_SPEED);
 
-//			motor_control_stop();
-			while(!((time_search + MS2ST(5000)) < chVTGetSystemTime())){ //searching a line for 5000ms
+		if(return_line_detected()){
+			right_motor_set_speed(0);
+			left_motor_set_speed(0);
+			send(PARK);
+		}
+		else if(chVTGetSystemTime() <= time_search + MS2ST(TIME_WAIT_SEARCHING_ROTA)){ //searching a line for 5000ms
+			right_motor_set_speed(0);
+			left_motor_set_speed(0);
+			send(RESEARCH_MVNT);
+		}
 
-				right_motor_set_speed(MOTOR_SPEED_LIMIT/4);
-				left_motor_set_speed(-MOTOR_SPEED_LIMIT/4);
-				if(return_line_detected()){
-					palTogglePad(GPIOD, GPIOD_LED1);	//activates LED if it finds a line
-					line_found = 1;
-					break;
-				}
-				chThdSleepUntilWindowed(time, time + MS2ST(100));
-			}
-			if(line_found){					//ligne trouvée, à remplir pour lancer le PID
-				search_line = 0;			//arrête de chercher la ligne
-				left_motor_set_speed(0);
-			}
-			else{
-				search_line = 0;				//stops searching line
-				set_time_running(5000);  		//random movements for x mx
-				set_motor_control_active(1);	//starts random movements
-			}
-    	}
-    	else{
-    	chThdSleepMilliseconds(200);
-    	}
+		chThdSleepMilliseconds(100);
+
     }
-
 }
 
 void find_line_start(){
@@ -75,11 +58,6 @@ void find_line_stop(){
 	findLineThd = NULL;
 
 	findLine_configured = 0;
-}
-
-void set_search_line(bool set_search_line){
-	search_line = set_search_line;
-	return;
 }
 
 
