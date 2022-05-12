72548fcdde73eeb390500856e8b5256ef1a524c7
Vassiliy Cheremetiev <vassiliy.rehacek@epfl.ch>
2022-05-12 10:19:36 +0200

    fonction distance

--- detect_proximity.h
+++ detect_proximity.h
@@ -6,5 +6,6 @@ void detect_proximity_stop(void);
 int return_wall_angle(void);
 bool return_wall_detected(void);
 int prox_value_delta(uint8_t sensor);
+float distance_value(void);
 
 #endif /* DETECT_PROXIMITY_H */
