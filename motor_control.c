aae251adab492dd3f26144fa87951ad79b515056
Nathan <nathan.benavides@epfl.ch>
2022-05-13 16:34:53 +0200

    voilà, c'est clean

--- motor_control.c
+++ motor_control.c
@@ -29,41 +29,41 @@ static THD_FUNCTION(MotorControl, arg) {
 
     	time = chVTGetSystemTime();
 
-		if(!return_wall_detected() || (abs(return_wall_angle()) > 90)){
-			right_motor_set_speed(MOTOR_SPEED_LIMIT/2);
-			left_motor_set_speed(MOTOR_SPEED_LIMIT/2);
+		if(!return_wall_detected() || (abs(return_wall_angle()) > ANGLE_BEHIND)){
+			right_motor_set_speed(MOTOR_SPEED);
+			left_motor_set_speed(MOTOR_SPEED);
 		}
-		else if((return_wall_angle() >= -10) && (return_wall_angle() <= 10)){
+		else if((return_wall_angle() >= -ANGLE_IN_FRONT) && (return_wall_angle() <= ANGLE_IN_FRONT)){
 			int random_number = rand()%2;
 			if(random_number == 0){
-				right_motor_set_speed(-MOTOR_SPEED_LIMIT/2);
-				left_motor_set_speed(MOTOR_SPEED_LIMIT/2);
-				chThdSleepUntilWindowed(time, time + MS2ST(500));
+				right_motor_set_speed(-MOTOR_SPEED);
+				left_motor_set_speed(MOTOR_SPEED);
+				chThdSleepUntilWindowed(time, time + MS2ST(MOTOR_CONTROL_ROTA_WAIT_TIME));
 			}
 			else {
-				right_motor_set_speed(MOTOR_SPEED_LIMIT/2);
-				left_motor_set_speed(-MOTOR_SPEED_LIMIT/2);
-				chThdSleepUntilWindowed(time, time + MS2ST(500));
+				right_motor_set_speed(MOTOR_SPEED);
+				left_motor_set_speed(-MOTOR_SPEED);
+				chThdSleepUntilWindowed(time, time + MS2ST(MOTOR_CONTROL_ROTA_WAIT_TIME));
 			}
 		}
 		else if(return_wall_angle() > 0 ){
-			if((return_wall_angle() == 90) && (prox_value_delta(5) > 150)){
-				right_motor_set_speed(-MOTOR_SPEED_LIMIT/2);
-				left_motor_set_speed(MOTOR_SPEED_LIMIT/2);
+			if((return_wall_angle() == 90) && (prox_value_delta(5) > PROX_VALUE_THRESHOLD)){
+				right_motor_set_speed(-MOTOR_SPEED);
+				left_motor_set_speed(MOTOR_SPEED);
 			}
-			right_motor_set_speed(MOTOR_SPEED_LIMIT/2 - MOTOR_SPEED_LIMIT/2 * (90 - return_wall_angle()));
-			left_motor_set_speed(MOTOR_SPEED_LIMIT/2 + MOTOR_SPEED_LIMIT/2 * (90 - return_wall_angle()));
+			right_motor_set_speed(MOTOR_SPEED - MOTOR_SPEED * (90 - return_wall_angle()));
+			left_motor_set_speed(MOTOR_SPEED + MOTOR_SPEED * (90 - return_wall_angle()));
 		}
 		else if(return_wall_angle() < 0){
-			if((return_wall_angle() == -90) && (prox_value_delta(2) > 150)){
-				right_motor_set_speed(MOTOR_SPEED_LIMIT/2);
-				left_motor_set_speed(-MOTOR_SPEED_LIMIT/2);
+			if((return_wall_angle() == -90) && (prox_value_delta(2) > PROX_VALUE_THRESHOLD)){
+				right_motor_set_speed(MOTOR_SPEED);
+				left_motor_set_speed(-MOTOR_SPEED);
 			}
-			right_motor_set_speed(MOTOR_SPEED_LIMIT/2 + MOTOR_SPEED_LIMIT/2 * (90 - return_wall_angle()));
-			left_motor_set_speed(MOTOR_SPEED_LIMIT/2 - MOTOR_SPEED_LIMIT/2 * (90 - return_wall_angle()));
+			right_motor_set_speed(MOTOR_SPEED + MOTOR_SPEED * (90 - return_wall_angle()));
+			left_motor_set_speed(MOTOR_SPEED - MOTOR_SPEED * (90 - return_wall_angle()));
 		}
 
-    	chThdSleepUntilWindowed(time, time + MS2ST(20));
+    	chThdSleepUntilWindowed(time, time + MS2ST(MOTOR_CONTROL_WAIT_TIME));
     }
 }
 
